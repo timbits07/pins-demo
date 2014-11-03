66176425c5ed09a775161ee13c2dfa940d838d13
timbits07 <tim.storey@yahoo.com>
2014-11-03 15:54:19 -0800

    added pagination

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -6,9 +6,8 @@ class PinsController < ApplicationController
   # GET /pins
   # GET /pins.json
   def index
-   @pins = Pin.all.order("created_at DESC")
- end
-
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
+  end
 
   # GET /pins/1
   # GET /pins/1.json
